d0fcfae0a8af2ebb61a02f8bd16cbbddfafce2a1
Amin NAIRI <nairi.amin@gmail.com>
2018-11-16 22:05:31 +0100

    :sparkles: linked the scroll event to hyperapp actions

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,21 @@ import { app } from 'hyperapp'
 import { state, actions } from './core'
 import { home } from './core/views'
 
+
 window.addEventListener('load', () => {
   const googleIcons = document.createElement('link')
   googleIcons.rel = 'stylesheet'
   googleIcons.href = 'https://fonts.googleapis.com/icon?family=Material+Icons'
   document.getElementsByTagName('head')[0].appendChild(googleIcons)
-  app(state, actions, home, document.body)
+  const main = app(state, actions, home, document.body)
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 64) {
+      // @ts-ignore
+      main.setBlueNavigationBar()
+    } else {
+      // @ts-ignore
+      main.setWhiteNavigationBar()
+    }
+  })
 })
