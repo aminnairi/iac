54756de2ca9eb94b05106e30be830b9bc4c78366
Amin NAIRI <nairi.amin@gmail.com>
2018-11-17 14:36:52 +0100

    :sparkles: added better error toast notification

--- src/core/actions.ts
+++ src/core/actions.ts
@@ -10,13 +10,33 @@ export const actions = {
       toast.dismiss()
     }
 
-    toast = M.toast({ html: input, classes: 'blue darken-3' })
+    toast = M.toast({
+      html: `<i class='material-icons'>info</i> &nbsp;${ input }`,
+      classes: 'blue darken-3'
+    })
+
+    return { toast }
+  },
+  error: () => ({ toast }) => {
+    if (toast && toast.timeRemaining > 0) {
+      toast.dismiss()
+    }
+
+    toast = M.toast({
+      html: `<i class='material-icons'>warning</i> &nbsp;Unable to fetch IP informations. Disable your ad blocker or try again`,
+      classes: 'red darken-3'
+    })
+
     return { toast }
   },
   fetchInformations: event => ({ ip }, actions) => {
     event.preventDefault()
     actions.notification('Fetching your IP informations...') 
-    fetch(`https://ipapi.co/${ip}/json/`, { mode: 'cors' }).then(response => response.json()).then(actions.setInformations).catch(error => M.toast({ html: error, classes: 'red' }))
+
+    fetch(`https://ipapi.co/${ip}/json/`, { mode: 'cors' })
+      .then(response => response.json())
+      .then(actions.setInformations)
+      .catch(actions.error)
   },
   setBlueNavigationBar: () => ({ theme: 'blue darken-3', textTheme: 'white-text' }),
   setWhiteNavigationBar: () => ({ theme: 'white', textTheme: 'blue-text text-darken-3' }),
