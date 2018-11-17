a09e51a2289d432d7066b0bfc3760b0b6f0a106f
Amin NAIRI <nairi.amin@gmail.com>
2018-11-17 08:52:12 +0100

    :sparkles: simplified export statement

--- src/utils/capitalize.ts
+++ src/utils/capitalize.ts
@@ -1,3 +1,1 @@
-const capitalize = input => input.split(' ').map(word => word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase()).join(' ')
-
-export { capitalize }
+export const capitalize = input => input.split(' ').map(word => word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase()).join(' ')
