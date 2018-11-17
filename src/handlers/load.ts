49b558b3e27d2ae0071191d86dc468d9fae524db
Amin NAIRI <nairi.amin@gmail.com>
2018-11-17 08:50:33 +0100

    :sparkles: using a util function to load stylesheet

--- src/handlers/load.ts
+++ src/handlers/load.ts
@@ -2,11 +2,10 @@ import { app } from 'hyperapp'
 import { state, actions } from '../core'
 import { home } from '../core/views'
 import { scroll } from './'
+import { stylesheet } from '../utils';
 
 export const load = () => {
-  const googleIcons = document.createElement('link')
-  googleIcons.rel = 'stylesheet'
-  googleIcons.href = 'https://fonts.googleapis.com/icon?family=Material+Icons'
+  const googleIcons = stylesheet('https://fonts.googleapis.com/icon?family=Material+Icons')
   document.getElementsByTagName('head')[0].appendChild(googleIcons)
   const main = app(state, actions, home, document.body)
 
