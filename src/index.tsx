0fc3265915e3855e207e7737dfecb5a35bbf61f5
Amin NAIRI <nairi.amin@gmail.com>
2018-11-17 08:44:27 +0100

    :building_construction: exported handlers

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,5 @@
 import './index.sass'
 
-import { app } from 'hyperapp'
-import { state, actions } from './core'
-import { home } from './core/views'
+import { load } from './handlers'
 
-
-window.addEventListener('load', () => {
-  const googleIcons = document.createElement('link')
-  googleIcons.rel = 'stylesheet'
-  googleIcons.href = 'https://fonts.googleapis.com/icon?family=Material+Icons'
-  document.getElementsByTagName('head')[0].appendChild(googleIcons)
-  const main = app(state, actions, home, document.body)
-
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 39.2) {
-      // @ts-ignore
-      main.setBlueNavigationBar()
-    } else {
-      // @ts-ignore
-      main.setWhiteNavigationBar()
-    }
-  })
-})
+window.addEventListener('load', load, false)
