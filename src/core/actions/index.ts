1bf6f86f0964bcf8eb0256edf1e959b2fb6410dd
Amin NAIRI <nairi.amin@gmail.com>
2018-11-18 10:36:39 +0100

    :building_construction: exported actions

--- src/core/actions/index.ts
+++ src/core/actions/index.ts
@@ -9,6 +9,8 @@ import { preventDefault } from './preventDefault'
 import { error } from './error'
 import { fetching } from './fetching'
 import { fetched } from './fetched'
+import { fetchInformations } from './fetchInformations'
+import { setBlueNavigationBar } from './setBlueNavigationBar'
 
 export const actions = {
   emptyIp,
@@ -20,42 +22,7 @@ export const actions = {
   error,
   fetching,
   fetched,
-  fetchInformations: event => async ({ ip }, actions) => {
-    event.preventDefault()
-    actions.fetching()
-
-    try {
-      const response = await fetch(`https://ipapi.co/${ip}/json/`, { mode: 'cors' })
-
-      if (response.status === 400) {
-        throw 'Bad request'
-      } else if (response.status === 404) {
-        throw 'URL not found'
-      } else if (response.status === 405) {
-        throw 'Method not allowed'
-      } else if (response.status === 429) {
-        throw 'Too many requests'
-      }
-
-      const json = await response.json()
-
-      if (json.error) {
-        actions.error(json.reason)
-      } else if (json.reserved) {
-        actions.error('Reserved IP adress')
-      } else {
-        actions.setInformations(json)
-      }
-    } catch (e) {
-      if (e instanceof TypeError) {
-        actions.error()
-      } else {
-        actions.error(e)
-      }
-    } finally {
-      actions.fetched()
-    }
-  },
-  setBlueNavigationBar: () => ({ theme: 'blue darken-3', textTheme: 'white-text' }),
+  fetchInformations,
+  setBlueNavigationBar,
   setWhiteNavigationBar: () => ({ theme: 'white', textTheme: 'blue-text text-darken-3' }),
 }
